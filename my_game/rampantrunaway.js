cf149f64514077caf8fa1f97f3693976bcd77bf1
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-11-08 17:54:51 -0500

    Update rampantrunaway.js
    Created config variable and onload function for new Phaser game.

---
+++ my_game/rampantrunaway.js
@@ -0,0 +1,17 @@
+var config = {
+    // width: ,
+    // height: ,
+    // backgroundColor: 0x000000,
+    // scene: [bootscreen, Level1, Level2, Level3],
+    // pixelArt: true,
+    // physics: {
+    //     default: "arcade",
+    //     arcade: {
+    //         debug: false
+    //     }
+    // }
+}
+
+window.onload = function() {
+    var game = new Phaser.Game(config);
+}
