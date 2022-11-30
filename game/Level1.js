c94f58cb3dc6a221914fe743e302273ab3ffcd1d
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-11-29 21:02:47 -0500

    Update Level1.js

--- game/Level1.js
+++ game/Level1.js
@@ -2,11 +2,13 @@ class Level1 extends Phaser.Scene {
   constructor() {
     super('playGameL1');
   }
+
   preload() {
     this.load.image('background', './assets/level1BG.jpg');
   }
+
   create() {
-    // Change background and border color
-    this.add.image(400, 300, 'background');
+    // Add background image to canvas
+    var bg = this.add.image(400, 300, 'background');
   }
 }
