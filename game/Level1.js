f0b948cb08930c1e84412c5e3ec5e3958dcbd571
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-11-29 20:49:23 -0500

    Update Level1.js
    bg preload and creation.

--- game/Level1.js
+++ game/Level1.js
@@ -2,11 +2,11 @@ class Level1 extends Phaser.Scene {
   constructor() {
     super('playGameL1');
   }
-  preload() {}
+  preload() {
+    this.load.image('background', './assets/level1BG.jpg');
+  }
   create() {
     // Change background and border color
-    var bg = document.getElementById('gameContainer');
-    bg.style.backgroundColor = '#000';
-    bg.style.border = '4px solid #EF971E';
+    this.add.image(400, 300, 'background');
   }
 }
